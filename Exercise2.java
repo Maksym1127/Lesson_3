a2eb77f62c92553e22f8ac2630380ade1f308e22
Maksym1127 <112717768+Maksym1127@users.noreply.github.com>
2022-09-09 00:12:13 +0300

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+public class Exercise2 {
+    public static void main(String[] args){
+        int[] intArray1 = {
+                1789, 2035, 1899, 1456, 2013,
+                1458, 2458, 1254, 1472, 2365,
+                1456, 2165, 1457, 2456};
+
+        int[] intArray2 = {
+                2333,442,5252,333,222,555,33,
+                334,777,666
+        };
+
+        String[] stringArray1 = {
+                "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C",
+                "C++"
+        };
+        String[] stringArray2 = {"abc",
+                "bc","ac","aac",
+                "agc","asc","adc",};
+        System.out.println();
+    }
+}
+
