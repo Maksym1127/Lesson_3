6ea3c5a91a3f7d1f4e806b1db9da0c4823ced497
Maksym1127 <maksimchuk004@gmail.com>
2022-09-09 00:28:17 +0300

    upd1

--- Exercise2.java
+++ Exercise2.java
@@ -1,15 +1,15 @@
 package lesson3;
 
 public class Exercise2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] intArray1 = {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2165, 1457, 2456};
 
         int[] intArray2 = {
-                2333,442,5252,333,222,555,33,
-                334,777,666
+                2333, 442, 5252, 333, 222, 555, 33,
+                334, 777, 666
         };
 
         String[] stringArray1 = {
@@ -21,9 +21,14 @@ public class Exercise2 {
                 "C++"
         };
         String[] stringArray2 = {"abc",
-                "bc","ac","aac",
-                "agc","asc","adc",};
-        System.out.println();
+                "bc", "ac", "aac",
+                "agc", "asc", "adc",};
+        int a = intArray1.length;
+        int b = intArray2.length;
+        int c = stringArray1.length;
+        int d = stringArray2.length;
+        System.out.println("Довжина масивів: 1: " + a + " 2: " + b + " 3: " + c + " 4: " + d);
+
     }
 }
 
